64bf88823e66db47721c0c3b320bdcf8193cef6f
Murat Kaya Aksoy <muratkaya@datasel.com.tr>
2018-02-23 18:35:43 +0300

    remove unusable comments

--- src/main/ConnectionParams.java
+++ src/main/ConnectionParams.java
@@ -6,20 +6,6 @@ import gnu.io.SerialPort;
 
 public class ConnectionParams implements Serializable {
 	
-//	DATABITS_5 = 5
-//			DATABITS_6 = 6
-//			DATABITS_7 = 7
-//			DATABITS_8 = 8
-//
-//			STOPBITS_1 = 1
-//			STOPBITS_2 = 2
-//			STOPBITS_3 = 1,5
-//
-//			PARITY_NONE = 0
-//			PARITY_ODD = 1
-//			PARITY_EVEN = 2
-//			PARITY_MARK = 3
-//			PARITY_SPACE = 4
 	
 	private static final long serialVersionUID = 1L;
 	
